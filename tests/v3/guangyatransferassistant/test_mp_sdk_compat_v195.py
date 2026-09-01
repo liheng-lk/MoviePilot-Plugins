36fa9417bf99d5523746cf0569fe3cd12723b9c5
liheng-lk <69590718+liheng-lk@users.noreply.github.com>
2026-09-02 01:03:55 +0800

    test: update SDK compatibility release marker

--- tests/v3/guangyatransferassistant/test_mp_sdk_compat_v195.py
+++ tests/v3/guangyatransferassistant/test_mp_sdk_compat_v195.py
@@ -21,5 +21,5 @@ def test_v195_keeps_runtime_fallback_without_internal_import():
 
 
 def test_v195_release_marker():
-    assert 'plugin_version = "1.10.5"' in ENTRY
-    assert 'build_id = "20260901-r16"' in ENTRY
+    assert 'plugin_version = "1.10.6"' in ENTRY
+    assert 'build_id = "20260902-r17"' in ENTRY
