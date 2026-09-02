795b64a1cb1e085300946e00c7675d0e8eeaf824
liheng-lk <69590718+liheng-lk@users.noreply.github.com>
2026-09-02 14:35:00 +0800

    test: bump release marker to 1.10.13

--- tests/v3/guangyatransferassistant/test_release_v1109_marker.py
+++ tests/v3/guangyatransferassistant/test_release_v1109_marker.py
@@ -5,5 +5,5 @@ ENTRY = (ROOT / "plugins.v3" / "guangyatransferassistant" / "__init__.py").read_
 
 
 def test_v1109_release_marker():
-    assert 'plugin_version = "1.10.12"' in ENTRY
-    assert 'build_id = "20260902-r23"' in ENTRY
+    assert 'plugin_version = "1.10.13"' in ENTRY
+    assert 'build_id = "20260902-r24"' in ENTRY
