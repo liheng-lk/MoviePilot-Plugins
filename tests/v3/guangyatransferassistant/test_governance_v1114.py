298ea89a422b1a9ceadffe9b0c129848f003cae6
liheng-lk <69590718+liheng-lk@users.noreply.github.com>
2026-09-11 23:42:45 +0800

    test(guangya): align console and channel consolidation contracts

--- tests/v3/guangyatransferassistant/test_governance_v1114.py
+++ tests/v3/guangyatransferassistant/test_governance_v1114.py
@@ -24,8 +24,8 @@ def test_governance_layer_parses_and_is_retained_beneath_final_xunlei_layer():
     ast.parse(UI_TEXT, filename=str(UI))
     assert "class GuangYaGovernanceV1114Mixin(GuangYaRuntimeFixV1113Mixin):" in TEXT
     assert "class GuangYaXunleiFinalV1114Mixin(GuangYaGovernanceV1114Mixin):" in FINAL_TEXT
-    assert "from .console_control_cursor_v1116 import GuangYaConsoleControlCursorV1116Mixin" in UI_TEXT
-    assert "class GuangYaGyingUiV1109Mixin(GuangYaConsoleControlCursorV1116Mixin):" in UI_TEXT
+    assert "from .console_control_v1116 import GuangYaConsoleControlV1116Mixin" in UI_TEXT
+    assert "class GuangYaGyingUiV1109Mixin(GuangYaConsoleControlV1116Mixin):" in UI_TEXT
     assert 'build_id = "20260902-r27"' in UI_TEXT
 
 
