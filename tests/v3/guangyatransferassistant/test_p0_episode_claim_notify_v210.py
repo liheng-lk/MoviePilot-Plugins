87301bc8efe0973e19d307d0a14a6c50ed6031f6
liheng-lk <69590718+liheng-lk@users.noreply.github.com>
2026-09-11 21:49:11 +0800

    test(guangya): align release markers with 2.0.13-r97

--- tests/v3/guangyatransferassistant/test_p0_episode_claim_notify_v210.py
+++ tests/v3/guangyatransferassistant/test_p0_episode_claim_notify_v210.py
@@ -101,8 +101,8 @@ def _ops_instance():
 
 
 def test_version_unchanged_r94():
-    assert 'plugin_version = "2.0.12"' in ENTRY
-    assert 'build_id = "20260911-r96"' in ENTRY
+    assert 'plugin_version = "2.0.13"' in ENTRY
+    assert 'build_id = "20260911-r97"' in ENTRY
 
 
 def test_a_first_cry_duplicate_episode_claim():
