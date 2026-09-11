d36bfa2e5b5a4a95c7bd5c03300a6c2838b7007f
liheng-lk <69590718+liheng-lk@users.noreply.github.com>
2026-09-11 21:48:39 +0800

    test(guangya): align release markers with 2.0.13-r97

--- tests/v3/guangyatransferassistant/test_core_pipeline_v11214.py
+++ tests/v3/guangyatransferassistant/test_core_pipeline_v11214.py
@@ -376,7 +376,7 @@ def test_every_storage_path_reuses_guangya_target_and_no_moviepilot_downloader()
 
 
 def test_current_public_release_is_v11214_after_full_gate_passes():
-    assert 'plugin_version = "2.0.12"' in ENTRY
+    assert 'plugin_version = "2.0.13"' in ENTRY
     assert 'plugin_version = "1.12.14"' in CORE
     assert 'build_id = "20260905-r60"' in FINAL
 
